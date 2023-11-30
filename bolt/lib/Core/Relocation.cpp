93748eba4765f4e94df84f7d8e9e8e46ba6abee9
zhaoqi <zhaoqi01@loongson.cn>
2023-11-30 10:04:56 +0800

    [BOLT][LoongArch] Implement R_LARCH_64
    Add R_LARCH_64 relocation support.
    
    Also ensure the 'reorder-data-writable-ptload' test pass on LoongArch.
    
    Change-Id: Ifd0bff87ccf223924a731f4be1349e1b6a765d77
    
    Signed-off-by: Rong Bao <rong.bao@csmantle.top>

--- bolt/lib/Core/Relocation.cpp
+++ bolt/lib/Core/Relocation.cpp
@@ -144,6 +144,7 @@ static bool isSupportedLoongArch(uint64_t Type) {
   case ELF::R_LARCH_GOT_PC_HI20:
   case ELF::R_LARCH_GOT64_PC_LO20:
   case ELF::R_LARCH_GOT64_PC_HI12:
+  case ELF::R_LARCH_64:
     return true;
   }
 }
@@ -270,6 +271,8 @@ static size_t getSizeForTypeLoongArch(uint64_t Type) {
   case ELF::R_LARCH_GOT64_PC_LO20:
   case ELF::R_LARCH_GOT64_PC_HI12:
     return 4;
+  case ELF::R_LARCH_64:
+    return 8;
   }
 }
 
@@ -566,6 +569,8 @@ static uint64_t extractValueLoongArch(uint64_t Type, uint64_t Contents,
   default:
     errs() << object::getELFRelocationTypeName(ELF::EM_LOONGARCH, Type) << '\n';
     llvm_unreachable("unsupported relocation type");
+  case ELF::R_LARCH_64:
+    return Contents;
   case ELF::R_LARCH_B26: {
     Contents &= ~0xfffffffffc000000ULL;
     uint64_t LowBits = (Contents >> 10) & 0xffff;
@@ -813,6 +818,7 @@ static bool isPCRelativeLoongArch(uint64_t Type) {
   switch (Type) {
   default:
     llvm_unreachable("Unknown relocation type");
+  case ELF::R_LARCH_64:
   case ELF::R_LARCH_PCALA_LO12:
   case ELF::R_LARCH_GOT_PC_LO12:
     return false;
@@ -1076,6 +1082,8 @@ uint32_t Relocation::getAbs64() {
     return ELF::R_AARCH64_ABS64;
   case Triple::riscv64:
     return ELF::R_RISCV_64;
+  case Triple::loongarch64:
+    return ELF::R_LARCH_64;
   case Triple::x86_64:
     return ELF::R_X86_64_64;
   }
