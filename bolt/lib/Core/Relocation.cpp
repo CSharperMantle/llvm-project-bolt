57ddae0f2655d1db26e495f692727b0332d9b32a
zhaoqi <zhaoqi01@loongson.cn>
2024-04-17 11:47:04 +0800

    [BOLT][LoongArch] Support for max-funcs option
    This enables --max-funcs=<N> option works properly.
    
    Test 'max-funcs.test' passed.
    
    Co-authored-by: Rong Bao <rong.bao@csmantle.top>
    Signed-off-by: Rong Bao <rong.bao@csmantle.top>

--- bolt/lib/Core/Relocation.cpp
+++ bolt/lib/Core/Relocation.cpp
@@ -390,6 +390,33 @@ static uint64_t encodeValueRISCV(uint32_t Type, uint64_t Value, uint64_t PC) {
   return Value;
 }
 
+static uint64_t canEncodeValueLoongArch(uint32_t Type, uint64_t Value,
+                                        uint64_t PC) {
+  switch (Type) {
+  default:
+    llvm_unreachable("unsupported relocation");
+  case ELF::R_LARCH_B26:
+    return true;
+  }
+}
+
+static uint64_t encodeValueLoongArch(uint64_t Type, uint64_t Value,
+                                     uint64_t PC) {
+  switch (Type) {
+  default:
+    llvm_unreachable("unsupported relocation");
+  case ELF::R_LARCH_B26:
+    Value -= PC;
+    assert(isInt<28>(Value) &&
+           "only PC + [-128MiB, +128MiB - 4] is allowed for direct call");
+    // Immediate goes in bits 25:0 of BL.
+    // OP 0101_01 goes in bits 31:26 of BL.
+    Value = ((Value >> 2) & 0x3ffffff) | 0x54000000ULL;
+    break;
+  }
+  return Value;
+}
+
 static uint64_t extractValueX86(uint32_t Type, uint64_t Contents, uint64_t PC) {
   if (Type == ELF::R_X86_64_32S)
     return SignExtend64<32>(Contents);
@@ -914,7 +941,7 @@ uint64_t Relocation::encodeValue(uint32_t Type, uint64_t Value, uint64_t PC) {
   case Triple::riscv64:
     return encodeValueRISCV(Type, Value, PC);
   case Triple::loongarch64:
-    llvm_unreachable("encodeValueLoongArch(Type, Value, PC);");
+    return encodeValueLoongArch(Type, Value, PC);
   case Triple::x86_64:
     return encodeValueX86(Type, Value, PC);
   }
@@ -928,6 +955,8 @@ bool Relocation::canEncodeValue(uint32_t Type, uint64_t Value, uint64_t PC) {
     return canEncodeValueAArch64(Type, Value, PC);
   case Triple::riscv64:
     return canEncodeValueRISCV(Type, Value, PC);
+  case Triple::loongarch64:
+    return canEncodeValueLoongArch(Type, Value, PC);
   case Triple::x86_64:
     return true;
   }
