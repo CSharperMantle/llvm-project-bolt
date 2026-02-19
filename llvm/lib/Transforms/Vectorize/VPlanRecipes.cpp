5eb9c3c14fec677f6679d860dd08310836ae94e0
Benjamin Maxwell <benjamin.maxwell@arm.com>
2026-02-19 15:29:32 +0000

    [LV] Add support for partial alias masking with tail folding
    This patch adds basic support for partial alias masking, which allows
    entering the vector loop even when there is aliasing within a single
    vector iteration. It does this by clamping the VF to the safe distance
    between pointers. This allows the runtime VF to be anywhere from 2 to
    the "static" VF.
    
    Conceptually, this transform looks like:
    
    ```
      // `c` and `b` may alias.
      for (int i = 0; i < n; i++) {
        c[i] = a[i] + b[i];
      }
    ```
    
    ->
    
    ```
      svbool_t alias_mask = loop.dependence.war.mask(b, c);
      int num_active = num_active_lanes(mask);
      if (num_active >= 2) {
        for (int i = 0; i < n; i += num_active) {
          // ... vector loop masked with `alias_mask`
        }
      }
      // ... scalar tail
    ```
    
    This initial patch has a number of limitations:
    
    - The loop must be tail-folded
      * We intend to follow-up with full alias-masking support for loops
        without tail-folding
    - The mask and transform is only valid for IC = 1
      * Some recipes may not handle the "ClampedVF" correctly at IC > 1
      * Note: On AArch64, we also only have native alias mask instructions
        for IC = 1
    - Reverse iteration is not supported
      * The mask reversal logic is not correct for the alias mask
        (or clamped ALM)
    - First order recurrences are not supported
      * The `splice.right` is not lowered correctly for clamped VFs
    - This style of vectorization is not enabled by default/costed
      * It can be enabled with `-force-partial-aliasing-vectorization`
      * When enabled, alias masking is used instead of the standard diff
        checks (when legal to do so)
    
    This PR supersedes #100579 (closes #100579).
    
    Rebase tests
    
    Fixups
    
    Fixups
    
    Fixups
    
    Fixups
    
    Fixups
    
    Fixups
    
    Add test
    
    Fixups
    
    Attach alias-mask early
    
    Use VPInstruction in preheader
    
    Fixups

--- llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanRecipes.cpp
@@ -492,6 +492,7 @@ unsigned VPInstruction::getNumOperandsForOpcode() const {
   switch (Opcode) {
   case VPInstruction::StepVector:
   case VPInstruction::VScale:
+  case VPInstruction::IncomingAliasMask:
     return 0;
   case Instruction::Alloca:
   case Instruction::ExtractValue:
@@ -509,6 +510,7 @@ unsigned VPInstruction::getNumOperandsForOpcode() const {
   case VPInstruction::ResumeForEpilogue:
   case VPInstruction::Reverse:
   case VPInstruction::Unpack:
+  case VPInstruction::NumActiveLanes:
     return 1;
   case Instruction::ICmp:
   case Instruction::FCmp:
@@ -654,6 +656,20 @@ Value *VPInstruction::generate(VPTransformState &State) {
     return Builder.CreateIntrinsic(Intrinsic::get_active_lane_mask,
                                    {PredTy, ScalarTC->getType()},
                                    {VIVElem0, ScalarTC}, nullptr, Name);
+  }
+  case VPInstruction::NumActiveLanes: {
+    Value *Op = State.get(getOperand(0));
+    auto *VecTy = cast<VectorType>(Op->getType());
+    assert(VecTy->getScalarSizeInBits() == 1 &&
+           "NumActiveLanes only implemented for i1 vectors");
+
+    Type *Ty = State.TypeAnalysis.inferScalarType(this);
+    Value *ZExt = Builder.CreateCast(
+        Instruction::ZExt, Op,
+        VectorType::get(Builder.getInt32Ty(), VecTy->getElementCount()));
+    Value *Count =
+        Builder.CreateUnaryIntrinsic(Intrinsic::vector_reduce_add, ZExt);
+    return Builder.CreateCast(Instruction::ZExt, Count, Ty, "num.active.lanes");
   }
   case VPInstruction::FirstOrderRecurrenceSplice: {
     // Generate code to combine the previous and current values in vector v3.
@@ -1317,7 +1333,8 @@ bool VPInstruction::isVectorToScalar() const {
          getOpcode() == VPInstruction::LastActiveLane ||
          getOpcode() == VPInstruction::ExtractLastActive ||
          getOpcode() == VPInstruction::ComputeReductionResult ||
-         getOpcode() == VPInstruction::AnyOf;
+         getOpcode() == VPInstruction::AnyOf ||
+         getOpcode() == VPInstruction::NumActiveLanes;
 }
 
 bool VPInstruction::isSingleScalar() const {
@@ -1394,6 +1411,7 @@ bool VPInstruction::opcodeMayReadOrWriteFromMemory() const {
   case VPInstruction::ExtractLastPart:
   case VPInstruction::ExtractPenultimateElement:
   case VPInstruction::ActiveLaneMask:
+  case VPInstruction::IncomingAliasMask:
   case VPInstruction::ExitingIVValue:
   case VPInstruction::ExplicitVectorLength:
   case VPInstruction::FirstActiveLane:
@@ -1514,6 +1532,9 @@ void VPInstruction::printRecipe(raw_ostream &O, const Twine &Indent,
   case VPInstruction::ActiveLaneMask:
     O << "active lane mask";
     break;
+  case VPInstruction::IncomingAliasMask:
+    O << "incoming-alias-mask";
+    break;
   case VPInstruction::ExplicitVectorLength:
     O << "EXPLICIT-VECTOR-LENGTH";
     break;
@@ -1600,6 +1621,9 @@ void VPInstruction::printRecipe(raw_ostream &O, const Twine &Indent,
     break;
   case VPInstruction::ExtractLastActive:
     O << "extract-last-active";
+    break;
+  case VPInstruction::NumActiveLanes:
+    O << "num-active-lanes";
     break;
   default:
     O << Instruction::getOpcodeName(getOpcode());
