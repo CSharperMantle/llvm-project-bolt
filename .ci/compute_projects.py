2add08a063ac657d3ef5499b830b0b8c9e8eaded
JP Lehr <JanPatrick.Lehr@amd.com>
2026-03-11 04:44:15 -0500

    Use offload target instead of check-offload
    Update to use the new offload target, which should *build* the
    offloading libraries only w/o running tests.

--- .ci/compute_projects.py
+++ .ci/compute_projects.py
@@ -165,7 +165,7 @@ PROJECT_CHECK_TARGETS = {
     "openmp": "check-openmp",
     "polly": "check-polly",
     "lit": "check-lit",
-    "offload": "check-offload",
+    "offload": "offload",
 }
 
 RUNTIMES = {
