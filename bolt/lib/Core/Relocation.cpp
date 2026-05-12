2b94f2a8e059c354eec653758b11151453c0a5f8
Rong Bao <rong.bao@csmantle.top>
2026-05-12 10:41:41 +0800

    [BOLT][LoongArch] Skip R_LARCH_{RELAX,ALIGN} relocs

--- bolt/lib/Core/Relocation.cpp
+++ bolt/lib/Core/Relocation.cpp
@@ -319,7 +319,14 @@ static bool skipRelocationTypeRISCV(uint32_t Type) {
 }
 
 static bool skipRelocationTypeLoongArch(uint32_t Type) {
-  return Type == ELF::R_LARCH_NONE;
+  switch (Type) {
+  default:
+    return false;
+  case ELF::R_LARCH_NONE:
+  case ELF::R_LARCH_RELAX:
+  case ELF::R_LARCH_ALIGN:
+    return true;
+  }
 }
 
 static uint64_t encodeValueX86(uint32_t Type, uint64_t Value, uint64_t PC) {
