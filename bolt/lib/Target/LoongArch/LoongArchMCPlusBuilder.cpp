75fc733c9b70ea6c1921147eb08573acd2660d39
Rong Bao <rong.bao@csmantle.top>
2026-05-12 19:18:22 +0800

    [BOLT][LoongArch] Reorder relocation enums by numerical encoding

--- bolt/lib/Target/LoongArch/LoongArchMCPlusBuilder.cpp
+++ bolt/lib/Target/LoongArch/LoongArchMCPlusBuilder.cpp
@@ -44,8 +44,6 @@ public:
     case ELF::R_LARCH_B16:
     case ELF::R_LARCH_B21:
     case ELF::R_LARCH_B26:
-    case ELF::R_LARCH_PCREL20_S2:
-    case ELF::R_LARCH_32_PCREL:
     case ELF::R_LARCH_PCALA_LO12:
     case ELF::R_LARCH_PCALA_HI20:
     case ELF::R_LARCH_PCALA64_LO20:
@@ -58,6 +56,8 @@ public:
     case ELF::R_LARCH_TLS_LE_LO12:
     case ELF::R_LARCH_TLS_IE_PC_HI20:
     case ELF::R_LARCH_TLS_IE_PC_LO12:
+    case ELF::R_LARCH_32_PCREL:
+    case ELF::R_LARCH_PCREL20_S2:
       return true;
     default:
       llvm_unreachable("Unexpected LoongArch relocation type in code");
@@ -432,8 +432,6 @@ public:
       return LoongArchMCExpr::create(Expr, ELF::R_LARCH_B21, Ctx);
     case ELF::R_LARCH_B26:
       return LoongArchMCExpr::create(Expr, ELF::R_LARCH_B26, Ctx);
-    case ELF::R_LARCH_PCREL20_S2:
-      return LoongArchMCExpr::create(Expr, ELF::R_LARCH_PCREL20_S2, Ctx);
     case ELF::R_LARCH_PCALA_LO12:
     case ELF::R_LARCH_GOT_PC_LO12:
     case ELF::R_LARCH_TLS_IE_PC_LO12:
@@ -452,6 +450,8 @@ public:
       return LoongArchMCExpr::create(Expr, ELF::R_LARCH_TLS_LE_HI20, Ctx);
     case ELF::R_LARCH_TLS_LE_LO12:
       return LoongArchMCExpr::create(Expr, ELF::R_LARCH_TLS_LE_LO12, Ctx);
+    case ELF::R_LARCH_PCREL20_S2:
+      return LoongArchMCExpr::create(Expr, ELF::R_LARCH_PCREL20_S2, Ctx);
     }
   }
 
