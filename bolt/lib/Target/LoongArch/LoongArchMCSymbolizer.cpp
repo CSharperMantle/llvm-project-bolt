32b4756704a27bb4f54377ae09eef5a2402bd485
Rong Bao <rong.bao@csmantle.top>
2026-05-13 11:06:19 +0800

    [BOLT][LoongArch] Implement TLS DESC relocations

--- bolt/lib/Target/LoongArch/LoongArchMCSymbolizer.cpp
+++ bolt/lib/Target/LoongArch/LoongArchMCSymbolizer.cpp
@@ -116,6 +116,23 @@ LoongArchMCSymbolizer::adjustRelocation(const Relocation &Rel,
       break;
     }
   }
+  if (Rel.Type == ELF::R_LARCH_TLS_DESC_PC_HI20) {
+    switch (Inst.getOpcode()) {
+    default:
+      return std::nullopt; // May be relaxed, can't adjust anyway
+    case LoongArch::PCALAU12I:
+      break;
+    }
+  }
+  if (Rel.Type == ELF::R_LARCH_TLS_DESC_PC_LO12) {
+    switch (Inst.getOpcode()) {
+    default:
+      return std::nullopt; // May be relaxed, can't adjust anyway
+    case LoongArch::ADDI_D:
+    case LoongArch::ADDI_W:
+      break;
+    }
+  }
 
   // Some linkers can relax a GOT reference into direct address
   // materialization without updating the relocation types.
